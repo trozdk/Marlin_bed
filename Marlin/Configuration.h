5e7a5f7a5b3ee98cd03bda797a4dd50d2096f275
trozdk <31743640+trozdk@users.noreply.github.com>
2017-09-08 17:54:43 +0200

    +anders

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -332,6 +332,8 @@
 //#define  DEFAULT_Kp 16.76
 //#define  DEFAULT_Ki 1.05
 //#define  DEFAULT_Kd 66.74
+
+// ANDERS
 #define  DEFAULT_Kp 27.27
 #define  DEFAULT_Ki 3.12
 #define  DEFAULT_Kd 59.66
@@ -376,8 +378,7 @@
   //#define  DEFAULT_bedKi 1.41
   //#define  DEFAULT_bedKd 1675.16
 
-
-  // TEVO Tarantula Custom PID Settings - Large Heatbead with Insulation
+// ANDERS
     #define  DEFAULT_bedKp 223.26
     #define  DEFAULT_bedKi 39.16
     #define  DEFAULT_bedKd 318.24
@@ -599,9 +600,12 @@ const bool Z_MIN_PROBE_ENDSTOP_INVERTING = true; // set to true to invert the lo
 //    |           |
 //    O-- FRONT --+
 //  (0,0)
+
+
+//ANDERS
 #define X_PROBE_OFFSET_FROM_EXTRUDER 25  // X offset: -left  +right  [of the nozzle]
 #define Y_PROBE_OFFSET_FROM_EXTRUDER 0  // Y offset: -front +behind [the nozzle]
-#define Z_PROBE_OFFSET_FROM_EXTRUDER 0   // Z offset: -below +above  [the nozzle]
+#define Z_PROBE_OFFSET_FROM_EXTRUDER -0.1   // Z offset: -below +above  [the nozzle]
 
 // X and Y axis travel speed (mm/m) between probes
 #define XY_PROBE_SPEED 8000
@@ -737,10 +741,11 @@ const bool Z_MIN_PROBE_ENDSTOP_INVERTING = true; // set to true to invert the lo
 
 // @section machine
 
+// ANDERS
 // Travel limits after homing (units are in mm)
 #define X_MIN_POS 8
 #define Y_MIN_POS 20
-#define Z_MIN_POS 0
+#define Z_MIN_POS -0.5
 #define X_MAX_POS 200
 #define Y_MAX_POS 210
 #define Z_MAX_POS 200
@@ -828,10 +833,11 @@ const bool Z_MIN_PROBE_ENDSTOP_INVERTING = true; // set to true to invert the lo
   #define ABL_GRID_POINTS_X 4
   #define ABL_GRID_POINTS_Y ABL_GRID_POINTS_X
 
+  // ANDERS
   // Set the boundaries for probing (where the probe can reach).
   #define LEFT_PROBE_BED_POSITION 33
   #define RIGHT_PROBE_BED_POSITION 200
-  #define FRONT_PROBE_BED_POSITION 20 
+  #define FRONT_PROBE_BED_POSITION 25 
   #define BACK_PROBE_BED_POSITION 205
 
   // The Z probe minimum outer margin (to validate G29 parameters).
